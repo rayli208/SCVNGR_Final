a01785e2250c9cdb3ebcf4973b15994936939552
Rayli Raykov <rayli208@gmail.com>
2019-03-19 15:25:33 -0400

    front end mobile jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,33 @@
-var drake = dragula([
-	document.getElementById('applied'),
-	document.getElementById('heardBack'),
-  document.getElementById('offer')
-])
+function checkSize(){
+  if ($(window).width() >= 920) {
+    $("#desktop-container").show();
+    $("#mobile-container").hide();
+  } else {
+    $("#desktop-container").hide();
+    $("#mobile-container").show();
+  }
+}
+checkSize();
 
-drake.on('drop', function(el, target, source, sibling) {
-  console.log("Started at: " + source.id + " Landed at:" + target.id + " Sibling " + el.previousSibling.id);
+$(document).ready(function() {
+  $(window).resize(function() {
+    checkSize()
+  });
+
+  var drake = dragula([
+    document.getElementById("applied"),
+    document.getElementById("heardBack"),
+    document.getElementById("offer")
+  ]);
+
+  drake.on("drop", function(el, target, source, sibling) {
+    console.log(
+      "Started at: " +
+        source.id +
+        " Landed at:" +
+        target.id +
+        " Sibling " +
+        el.previousSibling.id
+    );
+  });
 });
